ce81d473babad88cf5af75e0e2fca1fb676e69ea
odedg <odedg@wix.com>
2016-02-24 18:38:02 +0200

    support two levels render

--- src/js/components/breadcrumbs/breadCrumbs.jsx
+++ src/js/components/breadcrumbs/breadCrumbs.jsx
@@ -1,11 +1,14 @@
-define(['react'], function (React) {
+define(['lodash', 'react'], function (_, React) {
     'use strict';
     return React.createClass({
         displayName: 'BreadCrumbs',
+        renderItem: function (item) {
+            return <span>{item}</span>;
+        },
         render: function () {
             return (
                 <nav>
-                    <span>All Bookmarks</span>
+                    {_.map(this.props.items, this.renderItem)}
                 </nav>
             );
         }
--- src/tests/components/breadCrumbs.spec.jsx
+++ src/tests/components/breadCrumbs.spec.jsx
@@ -11,5 +11,15 @@ define(['react', 'reactDOM', 'components/breadcrumbs/breadCrumbs'],
                 var root = TestUtils.scryRenderedDOMComponentsWithTag(breadCrumbs, 'span')[0];
                 expect(root.innerHTML).toBe('All Bookmarks');
             });
+
+            it('should render two levels correctly', function () {
+                var breadCrumbs = TestUtils.renderIntoDocument(<BreadCrumbs items={['All Bookmarks', 'Current Group']}/>);
+                var items = TestUtils.scryRenderedDOMComponentsWithTag(breadCrumbs, 'span');
+                var root = items[0];
+                var current = items[1];
+
+                expect(root.innerHTML).toBe('All Bookmarks');
+                expect(current.innerHTML).toBe('Current Group');
+            });
         });
     });
