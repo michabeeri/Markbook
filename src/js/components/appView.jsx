8403f94a21eb1c68f838b0715e5498bc4fb3ce1e
vladale <@WSX3edc!>
2016-02-28 12:56:13 +0200

    addition of + button, wiring it up to opening of add bookmark modal
    Modal - modal basic component that adds modal behavior to its children
    
    AddBookmarkModal - component that defines children of 'Add Bookmark' modal
    
    ModalContainer - component that abstracts modals from application. It controls which modal is shown

--- src/js/components/appView.jsx
+++ src/js/components/appView.jsx
@@ -1,5 +1,5 @@
-define(['react', 'components/mainView/mainView', 'components/mainView/topbar', 'components/modals/AddBookmarkModal'],
-    function (React, MainView, TopBar, AddBookmarkModal) {
+define(['react', 'components/mainView/mainView', 'components/mainView/topbar', 'components/modals/ModalContainer', 'constants'],
+    function (React, MainView, TopBar, ModalContainer, constants) {
 
         'use strict';
         return React.createClass({
@@ -7,18 +7,31 @@ define(['react', 'components/mainView/mainView', 'components/mainView/topbar', '
             getInitialState: function () {
                 return {
                     items: this.props.items,
-                    username: 'user@wix.com'
+                    username: 'user@wix.com',
+                    openedModal: constants.eModalType.NONE
                 };
             },
             addItem: function () {
                 return false;
+            },
+            openAddBookMarkModal: function () {
+                this.setState({
+                    openedModal: constants.eModalType.MODAL_ADD_BOOKMARK
+                });
+            },
+            closeModal: function () {
+                this.setState({
+                    openedModal: constants.eModalType.NONE
+                });
             },
             render: function () {
                 return (
                     <div className='main'>
                         <TopBar username={this.state.username}/>
                         <MainView items={this.state.items}/>
-                        <AddBookmarkModal/>
+                        <ModalContainer closeModal={this.closeModal} openedModal={this.state.openedModal}/>
+                        <i className="fa fa-plus-circle fa-3x addBookmarkButton"
+                           onClick={this.openAddBookMarkModal}></i>
                     </div>
                 );
             }
