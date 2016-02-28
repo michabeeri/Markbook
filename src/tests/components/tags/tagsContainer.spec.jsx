61575df97baa4b01489801563adeeaaf7e0ac0ae
Naama Aharoni <naama.aharoni2@gmail.com>
2016-02-28 18:05:26 +0200

    Tags - update TagsLists and TagsContainer tests

--- src/tests/components/tags/tagsContainer.spec.jsx
+++ src/tests/components/tags/tagsContainer.spec.jsx
@@ -10,7 +10,7 @@ define(['React', 'components/tags/tagsContainer'],
             var tag = 'test';
             beforeEach(function () {
                 props = {
-                    tags: [],
+                    tags: ['tag1', 'tag2'],
                     addTag: function () {
                     },
                     removeTag: function () {
@@ -18,20 +18,41 @@ define(['React', 'components/tags/tagsContainer'],
                 };
             });
 
-            it('should call the addTag callback when a new tag is added', function () {
-                spyOn(props, 'addTag');
-                var instance = React.createElement(TagsContainer, props);
-                var tags = ReactTestUtils.renderIntoDocument(instance);
-                tags.addTag(tag);
-                expect(props.addTag).toHaveBeenCalledWith(tag);
+            describe('addTag', function () {
+
+                it('should call the addTag callback when a new tag is added', function () {
+                    spyOn(props, 'addTag');
+                    var instance = React.createElement(TagsContainer, props);
+                    var tags = ReactTestUtils.renderIntoDocument(instance);
+                    tags.addTag(tag);
+                    expect(props.addTag).toHaveBeenCalledWith(tag);
+                });
+
+                it('should update state when a new tag is added', function () {
+                    var instance = React.createElement(TagsContainer, props);
+                    var tags = ReactTestUtils.renderIntoDocument(instance);
+                    tags.addTag(tag);
+                    expect(tags.state.tags).toContain(tag);
+                });
+
             });
 
-            it('should call the removeTag callback when a tag is removed', function () {
-                spyOn(props, 'removeTag');
-                var instance = React.createElement(TagsContainer, props);
-                var tags = ReactTestUtils.renderIntoDocument(instance);
-                tags.removeTag(tag);
-                expect(props.removeTag).toHaveBeenCalledWith(tag);
+            describe('removeTag', function () {
+
+                it('should call the removeTag callback when a tag is removed', function () {
+                    spyOn(props, 'removeTag');
+                    var instance = React.createElement(TagsContainer, props);
+                    var tags = ReactTestUtils.renderIntoDocument(instance);
+                    tags.removeTag(tag);
+                    expect(props.removeTag).toHaveBeenCalledWith(tag);
+                });
+
+                it('should update state when a tag is removed', function () {
+                    var instance = React.createElement(TagsContainer, props);
+                    var tags = ReactTestUtils.renderIntoDocument(instance);
+                    tags.removeTag('tag1');
+                    expect(tags.state.tags).not.toContain('tag1');
+                });
             });
         })
     });
