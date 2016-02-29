5bdbcde3de2b4a1192bd1ad4def5b61ebbcce9fe
vladale <@WSX3edc!>
2016-02-29 17:40:38 +0200

    adding state to AddBookmarkModal

--- src/js/components/modals/AddBookmarkModal.jsx
+++ src/js/components/modals/AddBookmarkModal.jsx
@@ -2,26 +2,33 @@ define(['react', 'components/modals/Modal', 'actionProviders/actions'],
     function (React, Modal, actions) {
         'use strict';
 
+        var LinkedStateMixin = React.addons.LinkedStateMixin;
+
         return React.createClass({
+            mixins: [LinkedStateMixin],
             displayName: 'AddBookmarkModal',
             propTypes: {
                 classNameAddBookmark: React.PropTypes.string.isRequired,
                 closeModal: React.PropTypes.func.isRequired,
                 dispatch: React.PropTypes.func.isRequired
             },
+            getInitialState: function () {
+                return {
+                    bookmarkName: '',
+                    bookmarkUrl: ''
+                };
+            },
             addBookmark: function () {
-                var bookMarkName = this.refs.bookMarkName.value;
-                var bookMarkUrl = this.refs.bookMarkUrl.value;
-                this.props.dispatch(actions.addBookmark(bookMarkName, bookMarkUrl));
+                this.props.dispatch(actions.addBookmark(this.state.bookmarkName, this.state.bookmarkUrl));
                 this.props.closeModal();
             },
             render: function () {
                 return (
                     <Modal className={this.props.classNameAddBookmark} closeModal={this.props.closeModal}>
                         <h1>Add Bookmark</h1>
-                        <input type="text" ref="bookMarkName" placeholder="Name your bookmark"
+                        <input type="text" valueLink={this.linkState('bookmarkName')} placeholder="Name your bookmark"
                                className="input" autofocus/>
-                        <input type="text" ref="bookMarkUrl" placeholder="Paste url to bookmark"
+                        <input type="text" valueLink={this.linkState('bookmarkUrl')} placeholder="Paste url to bookmark"
                                className="input"/>
                         <button onClick={this.addBookmark} className="btn">Add Bookmark</button>
                     </Modal>
