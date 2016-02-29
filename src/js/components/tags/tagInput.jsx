309016865a0a69c9f10186fb5d8246e800f95699
Naama Aharoni <naama.aharoni2@gmail.com>
2016-02-29 16:59:10 +0200

    Tags - add dropdown to tagInput

--- src/js/components/tags/tagInput.jsx
+++ src/js/components/tags/tagInput.jsx
@@ -1,4 +1,5 @@
-define(['React'], function (React) {
+define(['React', 'lodash', 'components/dropdown/dropdown'],
+    function (React, _, DropDown) {
     'use strict';
 
     var LinkedStateMixin = React.addons.LinkedStateMixin;
@@ -14,20 +15,46 @@ define(['React'], function (React) {
             };
         },
         onKeyDown: function (event) {
-            if (event.keyCode === 13 || event.keyCode === 188) {
-                event.preventDefault();
-                this.props.addTag(this.state.input);
-                this.setState({
-                    input: ''
-                });
+            if (!_.isEmpty(event.target.value)) {
+                if (event.keyCode === 13 || event.keyCode === 188) {
+                    event.preventDefault();
+                    this.props.addTag(this.state.input);
+                    this.setState({
+                        input: ''
+                    });
+                }
             }
         },
+        filterTags: function (filter, tags) {
+            return _.filter(tags, function (tag) {
+                return tag.substring(0, filter.length) === filter;
+            });
+        },
+        getDataForDropdown: function (filter, tags) {
+            var filteredTags = this.filterTags(filter, tags);
+            var items = [{title: 'Tags', groupType: 'tag', lines: filteredTags}];
+            if (filteredTags.indexOf(filter) === -1) {
+                items.push({title: 'New tag', groupType: 'tag', lines: [filter]});
+            }
+
+            return {
+                input: filter,
+                items: items
+            };
+        },
         render: function () {
+            var dropdown = null;
+            if (!_.isEmpty(this.state.input)) {
+                dropdown = <DropDown ref="dropdown"
+                                     data={this.getDataForDropdown(this.state.input, this.props.tags)}
+                                     onLineClick={this.props.addTag}/>;
+            }
             return (
-                <span>
+                <div>
                     <span>Add tag:</span>
                     <input type="text" onKeyDown={this.onKeyDown} ref="input" valueLink={this.linkState('input')}/>
-                </span>
+                    {dropdown}
+                </div>
             );
         }
     });
