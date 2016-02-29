87bcf3b8cf243d36f1b74d7fe5659e35b892300d
Naama Aharoni <naama.aharoni2@gmail.com>
2016-02-29 17:10:30 +0200

    use TagsContainer in AddBookmarkModal

--- src/js/components/modals/AddBookmarkModal.jsx
+++ src/js/components/modals/AddBookmarkModal.jsx
@@ -1,5 +1,5 @@
-define(['react', 'components/modals/Modal', 'actionProviders/actions'],
-    function (React, Modal, actions) {
+define(['react', 'lodash', 'components/modals/Modal', 'actionProviders/actions', 'components/tags/tagsContainer'],
+    function (React, _, Modal, actions, TagsContainer) {
         'use strict';
 
         return React.createClass({
@@ -9,11 +9,28 @@ define(['react', 'components/modals/Modal', 'actionProviders/actions'],
                 closeModal: React.PropTypes.func.isRequired,
                 dispatch: React.PropTypes.func.isRequired
             },
+            getInitialState: function () {
+                return {
+                    tags: []
+                };
+            },
             addBookmark: function () {
                 var bookMarkName = this.refs.bookMarkName.value;
                 var bookMarkUrl = this.refs.bookMarkUrl.value;
                 this.props.dispatch(actions.addBookmark(bookMarkName, bookMarkUrl));
                 this.props.closeModal();
+            },
+            addTag: function (tag) {
+                this.state.tags.push(tag);
+                this.setState({
+                    tags: this.state.tags
+                });
+            },
+            removeTag: function (tag) {
+                _.pull(this.state.tags, tag);
+                this.setState({
+                    tags: this.state.tags
+                });
             },
             render: function () {
                 return (
@@ -23,6 +40,7 @@ define(['react', 'components/modals/Modal', 'actionProviders/actions'],
                                className="input" autofocus/>
                         <input type="text" ref="bookMarkUrl" placeholder="Paste url to bookmark"
                                className="input"/>
+                        <TagsContainer tags={this.state.tags} addTag={this.addTag} removeTag={this.removeTag}/>
                         <button onClick={this.addBookmark} className="btn">Add Bookmark</button>
                     </Modal>
                 );
