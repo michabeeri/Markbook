b721b26b0df47570bee31c85fb99955968f79553
Reut Savir <reutsa@wix.com>
2016-02-29 18:55:26 +0200

    Added bookmark list support to drag and drop

--- src/js/components/bookmarkList/bookmarkList.jsx
+++ src/js/components/bookmarkList/bookmarkList.jsx
@@ -5,6 +5,11 @@ define(['lodash', 'react', 'components/bookmarkList/bookmarkGroup', 'components/
 
         return React.createClass({
             displayName: 'BookmarkList',
+            getInitialState: function () {
+              return {
+                  dragged: null
+              }
+            },
             onView: function () {
                 window.open('http://www.google.com');
             },
@@ -19,6 +24,10 @@ define(['lodash', 'react', 'components/bookmarkList/bookmarkGroup', 'components/
                 }.bind(this);
             },
             createSingle: function (bm) {
+                var dragged = false;
+                if (bm.id === this.state.dragged) {
+                    dragged = true;
+                }
                 return (<Bookmark
                     key={bm.id}
                     dataId={'bm' + bm.id}
@@ -27,9 +36,16 @@ define(['lodash', 'react', 'components/bookmarkList/bookmarkGroup', 'components/
                     onEdit={this.dispatchActionGenerator(ActionProvider.editBookmark(bm.id))}
                     onDelete={this.dispatchActionGenerator(ActionProvider.removeBookmark(bm.id))}
                     onClick={this.onClickActionGenerator(bm.id)}
-                    onDoubleClick={this.onView}/>);
+                    onDoubleClick={this.onView}
+                    dragClass={dragged}
+                    onDragStart={this.setDragged}
+                    onDragOver={this.dragReorder}/>);
             },
             createGroup: function (bm) {
+                var dragged = false;
+                if (bm.id === this.state.dragged) {
+                    dragged = true;
+                }
                 return (<BookmarkGroup
                     key={bm.id}
                     dataId={'bm' + bm.id}
@@ -38,7 +54,10 @@ define(['lodash', 'react', 'components/bookmarkList/bookmarkGroup', 'components/
                     onEdit={this.dispatchActionGenerator(ActionProvider.editBookmark(bm.id))}
                     onDelete={this.dispatchActionGenerator(ActionProvider.removeBookmark(bm.id))}
                     onClick={this.onClickActionGenerator(bm.id)}
-                    onDoubleClick={this.dispatchActionGenerator(ActionProvider.openBookmarkGroup(bm.id))}/>);
+                    onDoubleClick={this.dispatchActionGenerator(ActionProvider.openBookmarkGroup(bm.id))}
+                    dragClass={dragged}
+                    onDragStart={this.setDragged}
+                    onDragOver={this.dragReorder}/>);
             },
             getFilteredBookmarks: function () {
                 var filter = this.props.state.filter;
@@ -54,6 +73,23 @@ define(['lodash', 'react', 'components/bookmarkList/bookmarkGroup', 'components/
                 return _.filter(this.props.state.bookmarks, function (bm) {
                     return titleCondition(bm) && tagCondition(bm);
                 });
+            },
+            dragReorder: function (draggedOverId) {
+                if (!this.state.dragged || this.state.dragged === draggedOverId) {
+                    return;
+                }
+                var indexDragged = _.findIndex(this.state.boxes, {key: this.state.dragged});
+                var indexDraggedOn = _.findIndex(this.state.boxes, {key: draggedOverId});
+                var newBoxes = this.state.boxes.slice();
+                newBoxes.splice(indexDraggedOn, 0, newBoxes.splice(indexDragged, 1)[0]);
+                this.setState({
+                    boxes: newBoxes
+                });
+            },
+            setDragged: function (draggedId) {
+                if (this.state.dragged !== draggedId) {
+                    this.setState({dragged: draggedId});
+                }
             },
             render: function () {
                 return (
