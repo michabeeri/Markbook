d552b3aa9007723ddd643f36a9c211612ec1f322
Naama Aharoni <naama.aharoni2@gmail.com>
2016-03-01 17:32:19 +0200

    Tags - pass filtered tags to suggestion dropdown

--- src/js/components/tags/tagInput.jsx
+++ src/js/components/tags/tagInput.jsx
@@ -2,63 +2,45 @@ define(['React', 'lodash', 'components/dropdown/dropdown'],
     function (React, _, DropDown) {
     'use strict';
 
-    var LinkedStateMixin = React.addons.LinkedStateMixin;
-
     return React.createClass({
         propTypes: {
             addTag: React.PropTypes.func.isRequired,
-            userTags: React.PropTypes.array.isRequired
-        },
-        mixins: [LinkedStateMixin],
-        getInitialState: function () {
-            return {
-                input: ''
-            };
-        },
-        addTag: function (tag) {
-            this.props.addTag(tag);
-            this.setState({
-                input: ''
-            });
+            valueLink: React.PropTypes.func.isRequired,
+            suggestedTags: React.PropTypes.array.isRequired
         },
         onKeyDown: function (event) {
-            if (!_.isEmpty(event.target.value)) {
-                if (event.keyCode === 13 || event.keyCode === 188) {
+            var tag = event.target.value;
+            if (!_.isEmpty(tag)) {
+                if (event.keyCode === 13) {
                     event.preventDefault();
-                    this.addTag(this.state.input);
+                    this.props.addTag(tag);
                 }
             }
         },
-        filterTags: function (filter, tags) {
-            return _.filter(tags, function (tag) {
-                return tag.substring(0, filter.length) === filter;
-            });
-        },
-        getDataForDropdown: function (filter, tags) {
-            var filteredTags = this.filterTags(filter, tags);
-            var items = [{title: 'Tags', groupType: 'tag', lines: filteredTags}];
-            if (filteredTags.indexOf(filter) === -1) {
-                items.push({title: 'New tag', groupType: 'tag', lines: [filter]});
-            }
-            return {
+        mapDataForDropdown: function (filter, filteredUserTags) {
+            var data = {
                 input: filter,
-                items: items
+                items: [{title: 'Tags', groupType: 'tag', lines: filteredUserTags}]
             };
+            if (filteredUserTags.indexOf(filter) === -1) {
+                data.items.push({title: 'New tag', groupType: 'tag', lines: [filter]});
+            }
+            return data;
         },
         onLineClick: function (group, tag) {
-            this.addTag(tag);
+            this.props.addTag(tag);
         },
         render: function () {
             var dropdown = null;
-            if (!_.isEmpty(this.state.input)) {
+            if (!_.isEmpty(this.refs.input)) {
                 dropdown = <DropDown ref="dropdown"
-                                     data={this.getDataForDropdown(this.state.input, this.props.userTags)}
+                                     data={this.mapDataForDropdown(this.props.input, this.props.suggestedTags)}
                                      onLineClick={this.onLineClick}/>;
             }
             return (
                 <div>
                     <span>Add tag:</span>
-                    <input type="text" onKeyDown={this.onKeyDown} ref="input" valueLink={this.linkState('input')}/>
+                    <input type="text" onKeyDown={this.onKeyDown} ref="input" valueLink={this.props.valueLink}/>
                     {dropdown}
                 </div>
             );
