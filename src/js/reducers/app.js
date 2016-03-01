4a6ef80f063a2d375f49ce834ab6ecf88433c1f2
Liran Kurtz <lirank@wix.com>
2016-03-01 18:56:29 +0200

    Added the redux-simple-router :)

--- src/js/reducers/app.js
+++ src/js/reducers/app.js
@@ -1,11 +1,12 @@
-define(['redux', 'reducers/bookmarks', 'reducers/userInfo', 'reducers/filter', 'reducers/currentBookmarkPath'],
-    function (Redux, bookmarksReducer, userInfoReducer, filterReducer, currentBookmarkPathReducer) {
+define(['redux', 'reducers/bookmarks', 'reducers/userInfo', 'reducers/filter', 'reducers/currentBookmarkPath', 'ReduxSimpleRouter'],
+    function (Redux, bookmarksReducer, userInfoReducer, filterReducer, currentBookmarkPathReducer, ReduxSimpleRouter) {
     'use strict';
 
     return Redux.combineReducers({
         bookmarks: bookmarksReducer,
         userInfo: userInfoReducer,
         filter: filterReducer,
-        currentBookmarkPath: currentBookmarkPathReducer
+        currentBookmarkPath: currentBookmarkPathReducer,
+        routing: ReduxSimpleRouter.routeReducer
     });
 });
