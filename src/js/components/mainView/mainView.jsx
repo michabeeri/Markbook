c00415afbeaa26791783c8a68e90a1dbf9340183
vladale <@WSX3edc!>
2016-03-03 12:20:46 +0200

    empty state

--- src/js/components/mainView/mainView.jsx
+++ src/js/components/mainView/mainView.jsx
@@ -39,13 +39,30 @@ define(['react', 'components/toolbar/toolbar', 'components/bookmarkList/bookmark
                 });
             },
             render: function () {
+                var content;
+
+                if (this.props.state.bookmarks.length === 1) { //root group
+                    content = (
+                        <div>
+                            <h1>Welcome</h1>
+                            <i className="fa fa-plus fa-3x"></i>
+                            <p>Add your first bookmark</p>
+                        </div>);
+                } else {
+                    content = (
+                        <div>
+                            <ToolBar {...this.props} layout={this.state.layout} switchLayout={this.switchLayout}/>
+                            {this.getBreadCrumbsComponent()}
+                            <BookmarkList dispatch={this.props.dispatch} state={this.props.state}
+                                          layout={this.state.layout}
+                                          modalUtils={{lastItemInGroup: this.openRemoveLastItemInGroupModal, groupDelete: this.openGroupDeleteModal}}/>
+                        </div>);
+                }
+
+
                 return (
                     <div>
-                        <ToolBar {...this.props} layout={this.state.layout} switchLayout={this.switchLayout}/>
-                        {this.getBreadCrumbsComponent()}
-                        <BookmarkList dispatch={this.props.dispatch} state={this.props.state}
-                                      layout={this.state.layout}
-                                      modalUtils={{lastItemInGroup: this.openRemoveLastItemInGroupModal, groupDelete: this.openGroupDeleteModal}}/>
+                        {content}
                         <ModalContainer dispatch={this.props.dispatch} state={this.props.state}/>
                         <i className="fa fa-plus-circle fa-3x btn-add" onClick={this.openAddBookMarkModal}></i>
                     </div>
