8bcdeb9e7f3f97af19cf29513869e93242b143b5
vladale <@WSX3edc!>
2016-03-08 15:09:33 +0200

    overlay

--- src/js/components/modals/Overlay.jsx
+++ src/js/components/modals/Overlay.jsx
@@ -4,14 +4,18 @@ define(['react'],
 
         return React.createClass({
             displayName: 'Overlay',
+            propTypes: {
+                children: React.PropTypes.node.isRequired
+            },
             preventAllInteractions: function (event) {
                 event.stopPropagation();
                 event.preventDefault();
             },
             render: function () {
                 return (
-                    <div className='overlay' onClick={this.preventAllInteractions}
+                    <div className='overlay fixed-center' onClick={this.preventAllInteractions}
                          onScroll={this.preventAllInteractions} onWheel={this.preventAllInteractions}>
+                        {this.props.children}
                     </div>
                 );
             }
