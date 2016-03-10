789e2ee654bc9f838d8e20de67d1ba76a49a3644
Naama Aharoni <naama.aharoni2@gmail.com>
2016-03-10 10:20:33 +0200

    update empty state to include panda and animation

--- src/js/components/mainView/mainViewEmptyState.jsx
+++ src/js/components/mainView/mainViewEmptyState.jsx
@@ -2,11 +2,15 @@ define(['react'], function (React) {
     'use strict';
 
     return React.createClass({
+        propTypes: {
+            openAddBookMarkModal: React.PropTypes.func.isRequired
+        },
         render: function () {
             return (
-                <div className = 'empty-state-container fixed-center'>
+                <div className = 'empty-state-container'>
                     <h1 className = 'empty-state-container-title'>Welcome</h1>
-                    <img src='img/bookmark.png' alt='bookmark' width='200' height='200'/>
+                    <img src='img/panda.png' alt='bookmark' width='300' height="310"/>
+                    <a className="btn btn-add" onClick={this.props.openAddBookMarkModal}><i className="fa fa-plus-circle"></i></a>
                     <p>Add your first bookmark</p>
                 </div>
             );
